ba466aa32a8998208a62191946ce1527949f341f
bananus25 <malikov.mark20@gmail.com>
2024-11-05 00:00:56 +0500

    Update index.html

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const https = require('https');
 const fs = require('fs');
+const https = require('https');
 
-const PORT = 3000;
+const HTTP_PORT = 3000;
+const HTTPS_PORT = 443;
 
 // Устанавливаем статическую папку
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Маршрут для главной страницы
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Запуск сервера HTTP
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+app.listen(HTTP_PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${HTTP_PORT}`);
 });
 
 // Запуск сервера HTTPS
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('key.posn'),
+  cert: fs.readFileSync('cert.posn')
 };
 
-https.createServer(options, app).listen(443, () => {
-  console.log('Сервер запущен на https://localhost');
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+  console.log(`Сервер запущен на https://localhost:${HTTPS_PORT}`);
 });
